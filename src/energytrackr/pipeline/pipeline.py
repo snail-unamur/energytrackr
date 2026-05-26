f4762db74cfc1bf949f7cd782c58c8c9c39b8116
JeromeMaquoi <33390494+JeromeMaquoi@users.noreply.github.com>
2026-05-26 13:06:42 +0200

    feat (pipeline): add set java version to measurements

--- src/energytrackr/pipeline/pipeline.py
+++ src/energytrackr/pipeline/pipeline.py
@@ -43,7 +43,7 @@ pre_test_stages: list[PipelineStage] = [
     CopyDirectoryStage(),
     SetDirectoryStage(),
     CheckoutStage(),
-    # JavaSetupStage(),
+    JavaSetupStage(),
     BuildStage(),
 ]
 
