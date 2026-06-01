7b8975b4d1c2e61f36501b31e85d10be5262779f
JeromeMaquoi <33390494+JeromeMaquoi@users.noreply.github.com>
2026-06-01 09:34:11 +0200

    Revert "fix (build): removed test verify stage"
    This reverts commit 97e880c72ab4359a5625225f48a95c0d91386ae8.

--- src/energytrackr/pipeline/pipeline.py
+++ src/energytrackr/pipeline/pipeline.py
@@ -21,6 +21,7 @@ from energytrackr.config.config_model import PipelineConfig
 from energytrackr.config.config_store import Config
 from energytrackr.config.loader import load_pipeline_config
 from energytrackr.pipeline.core_stages.build_stage import BuildStage
+from energytrackr.pipeline.core_stages.test_verify_stage import TestVerifyStage
 from energytrackr.pipeline.core_stages.checkout_stage import CheckoutStage
 from energytrackr.pipeline.core_stages.copy_directory_stage import CopyDirectoryStage
 from energytrackr.pipeline.core_stages.filter_and_regression_stage import FilterAndRegressionStage
@@ -45,6 +46,7 @@ pre_test_stages: list[PipelineStage] = [
     CheckoutStage(),
     JavaSetupStage(),
     BuildStage(),
+    TestVerifyStage(),
 ]
 
 batch_stages: list[PipelineStage] = [
