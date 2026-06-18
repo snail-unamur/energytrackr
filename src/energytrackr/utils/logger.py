4d7c04f88890e5da3bfad7da4cba4c691c91c427
JeromeMaquoi <33390494+JeromeMaquoi@users.noreply.github.com>
2026-06-18 18:08:47 +0200

    Set logger save to file to debug

--- src/energytrackr/utils/logger.py
+++ src/energytrackr/utils/logger.py
@@ -168,7 +168,7 @@ if not logger.handlers:
         log_path = os.path.join(LOG_DIR, f"debug_{timestamp}.log")
 
         file_handler = logging.FileHandler(log_path)
-        file_handler.setLevel(logging.WARNING)
+        file_handler.setLevel(logging.DEBUG)
         file_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
         file_handler.setFormatter(file_formatter)
         logger.addHandler(file_handler)
